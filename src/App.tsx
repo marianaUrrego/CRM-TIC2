e400e5a3371ddb8d43940a98b75eba89626fcf8c
sebas_vitz <127982375+sebasvitz@users.noreply.github.com>
2026-03-12 18:31:24 -0500

    Revert "Ruta del login al dashboard"

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./app/pages/Login";
 import Register from "./app/pages/Register";
-import Dashboard from "./app/pages/Dashboard";
 
 export default function App() {
   return (
@@ -9,7 +8,6 @@ export default function App() {
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
 
       {/* fallback */}
       <Route path="*" element={<Navigate to="/login" replace />} />
